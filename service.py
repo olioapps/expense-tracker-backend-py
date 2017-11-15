011b4de2654223ea7248c5aac55a483dafe15d32
Siggy Hinds <siggy.hinds@olioapps.com>
2017-11-14 18:39:18 -0800

    start of user and login endpoints

--- service.py
+++ service.py
@@ -9,27 +9,50 @@ class Service:
         print("constructed")
 
     @app.route('/')
-    def index():
+    def index(self):
         return "Hello World!"
 
     @app.route('/api/v2/mydata')
-    def hello():
+    def hello(self):
         return "Yo World!"
 
-    @app.route("/clients/")
-    def clients(self):
-        return 'The Clients Summary Page'
 
-    # user endpoints
+    ### User endpoints
+
+    @app.route("/user/create<User>")
+    def create_user(self, User):
+        return 'create new user: %s' % User
+
+    # @app.route("/user/delete<userID>")
+    # def delete_user(self, userID):
+    #     return 'delete user: %s' % userID
+
     @app.route("/user/<username>")
-    def show_user_profile(username):
+    def show_user_profile(self, username):
+        '''return user profile for username '''
         return 'Username: %s' % username
 
-    @app.route("/getUser/")
-    def get_user():
-        '''return username of all users '''
+    @app.route("/get_users/")
+    def get_users(self):
+        '''return user info for all users '''
+
+    @app.route("/user/edit<User>")
+    def edit_user(self, User):
+        return 'edit user: %s' % User
 
 
+    ### Login endpoints
+
+    @app.route("/login/")
+    def login(self):
+        return 'The Login Page'
+
+    @app.route("/login/submit/<username><pin>")
+    def submit_login(self, username, pin):
+        return 'edit user: %s %s' % username, pin
+
+
+    ### R U N
 
     def run(self):
         app.run(host="0.0.0.0")
